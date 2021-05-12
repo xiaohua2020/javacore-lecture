ed2f964e3c364e65cc897edf9c839cc49948ce95
xiaohua <834133404@qq.com>
2021-05-12 10:36:42 +0800

    push commit

--- src/main/java/com/atguigu/java/collection/Test01.java
+++ src/main/java/com/atguigu/java/collection/Test01.java
@@ -12,5 +12,6 @@ public class Test01 {
         System.out.println("hello zhangsan");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
