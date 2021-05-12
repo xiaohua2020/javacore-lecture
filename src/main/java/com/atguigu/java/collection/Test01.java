ca1171aa94df16208d72826bf64da982771e28f9
xiaohua <834133404@qq.com>
2021-05-12 09:58:20 +0800

    second commit

--- src/main/java/com/atguigu/java/collection/Test01.java
+++ src/main/java/com/atguigu/java/collection/Test01.java
@@ -7,5 +7,6 @@ package com.atguigu.java.collection;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 }
